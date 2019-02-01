04ec4744d0c573dc377dac262c4ad1e85a61b6b2
Lech H. Conde <lherre36@ford.com>
2019-02-01 14:28:07 -0500

    Initial commit

---
+++ UpcomingMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpcomingMovies
+//
+//  Created by Lech H. Conde on 01/02/2019.
+//  Copyright © 2019 Lech H. Conde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
