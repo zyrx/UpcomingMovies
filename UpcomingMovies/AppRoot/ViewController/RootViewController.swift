3be3c9268dd9c478478906eb1ecb440d91ee76c1
Lech H. Conde <lherre36@ford.com>
2019-02-08 02:48:21 -0500

    Added RootViewController & Configuration Service implementation

---
+++ UpcomingMovies/AppRoot/ViewController/RootViewController.swift
@@ -0,0 +1,59 @@
+//
+//  RootViewController.swift
+//  UpcomingMovies
+//
+//  Created by Lech H. Conde on 08/02/2019.
+//  Copyright © 2019 Lech H. Conde. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class RootViewController: BaseViewController<RootView> {
+    
+    let viewModel: ConfigurationViewable
+    
+    // MARK: - Initialization
+    required init(viewModel: ConfigurationViewable) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    convenience override init() {
+        self.init(viewModel: ConfigurationViewModel())
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        bindViewModel()
+        viewModel.getConfiguration()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        mainView.activityIndicator.startAnimating()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        mainView.activityIndicator.stopAnimating()
+    }
+    
+    private func setupView() {}
+    
+    private func bindViewModel() {
+        viewModel.configuration.bind { configuration in
+            guard let configuration = configuration else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                let moviesViewController = MoviesViewController(with: configuration)
+                let moviesNavigationController = UINavigationController(rootViewController: moviesViewController)
+                moviesNavigationController.modalTransitionStyle = .crossDissolve
+                self?.present(moviesNavigationController, animated: true, completion: nil)
+            }
+        }
+    }
+}
