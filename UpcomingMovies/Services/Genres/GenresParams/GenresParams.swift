c751b27e7bac2444ab34f55ee836747d960c21a1
Lech H. Conde <lech.hconde@gmail.com>
2019-02-08 15:10:51 -0500

    Added License Notice

--- UpcomingMovies/Services/Genres/GenresParams/GenresParams.swift
+++ UpcomingMovies/Services/Genres/GenresParams/GenresParams.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Lech H. Conde on 06/02/2019.
 //  Copyright © 2019 Lech H. Conde. All rights reserved.
+//  GNU General Public License version 3 or later; see LICENSE or <https://www.gnu.org/licenses/>.
 //
 
 import Foundation
